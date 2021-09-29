ca2bc2f523232d4c3e3df313b86374b7e1f2b122
Adam McCoy <adam.mccoy31@gmail.com>
2021-09-28 19:56:44 -0600

    inil

--- pwa/Budget-Tracker-PWA/public/js/service-worker.js
+++ pwa/Budget-Tracker-PWA/public/js/service-worker.js
@@ -1,8 +1,6 @@
-// global constants 
 const APP_PREFIX = 'FoodFest-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
-
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
@@ -19,8 +17,6 @@ const FILES_TO_CACHE = [
   '/icons/icon-384x384.png',
   '/icons/icon-512x512.png'  
 ];
-
-// adding files to the precache so that the application can use the cache (installing service worker)
 self.addEventListener('install', function (e) {
   e.waitUntil(
       caches.open(CACHE_NAME).then(function (cache) {
@@ -29,16 +25,13 @@ self.addEventListener('install', function (e) {
       })
     )
 });
-
-// activate a service worker
 self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
       let cacheKeeplist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX);
       });
-          cacheKeeplist.push(CACHE_NAME);
-          // returns a promise that resolves once all old versions of the cache have been deleted 
+          cacheKeeplist.push(CACHE_NAME); 
           return Promise.all(keyList.map(function (key, i) {
               if (cacheKeeplist.indexOf(key) === -1) {
               console.log('deleting cache : ' + keyList[i] );
@@ -49,24 +42,17 @@ self.addEventListener('activate', function (e) {
   })
   )
 });
-
-// retrieving information from the cache 
 self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
   e.respondWith(
     caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
+      if (request) {
         console.log('responding with cache : ' + e.request.url)
-      //   console.log(e.request.url)
         return request
-      } else {       // if there are no cache, try fetching request
+      } else {
         console.log('file is not cached, fetching : ' + e.request.url)
-      //   console.log(e.request.url)
         return fetch(e.request)
       }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
     })
   )
 });
